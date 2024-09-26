5b6393e91d0369e8d11973f3c12dcdbfe88f40e1
annescharf <ascharf@ab.mpg.de>
2024-09-26 21:04:15 +0200

    update to move2

--- README.md
+++ README.md
@@ -1,33 +1,46 @@
-## Morning Report pdf Overview
+## Morning Report Overview Table PDF
 MoveApps
 
 Github repository: *github.com/movestore/MorningRep_Overview*
 
 ## Description
-This App provides the Morning Report Overview as PDF download file. Given are for each individual the timestamps of the first and last deployed position, the last tag voltage, the moved distance and number of positions during the last 24h and 7d and an indication of migration or death event during the last 7 days. All you need when in the field looking for your animals. 
+This App provides the Morning Report Overview table as a PDF and csv download file. Given are for each track the timestamps of the first and last deployed position, the last tag voltage, the moved distance and number of positions during the last 24hours and 7days and an indication of migration or death event during the last 7 days. All you need when in the field looking for your animals. 
 
 ## Documentation
-An overview table is generated as PDF download: For each animal and tag the table shows the first and last timestamp, the last timestamp in local timezone (for convenience in the field), the last tag volatage and the number of positions and distance travelled during the last 24 hours or 7 days, respectively. If the animal was migrating, dead or had no data during the last 7 days then this event would be indicated.
+An overview table is generated as PDF and csv download: For each animal and tag the table shows the first and last timestamp, the last timestamp in local timezone (for convenience in the field), the last tag volatage and the number of positions and distance traveled during the last 24 hours or 7 days, respectively. If the animal was migrating, dead or had no data during the last 7 days then this event will be indicated.
 
-### Input data
-moveStack in Movebank format
+### Application scope
+#### Generality of App usability
+This App was developed for any taxonomic group. 
 
-### Output data
-moveStack in Movebank format
+#### Required data properties
+The App should work for any kind of (location) data. Specially useful for live feed data.
+
+### Input type
+`move2::move2_loc`
+
+### Output type
+`move2::move2_loc`
 
 ### Artefacts
 `MorningReport_overviewTable.pdf`: A simple overview table of the above specified data properties for each animal (row).
+`MorningReport_overviewTable.csv`: A simple overview table of the above specified data properties for each animal (row).
 
-### Settings
-**Reference time (`time_now`):** reference timestamp towards which all analyses are performed. Generally (and by default) this is NOW, especially if in the field and looking for one or the other animal or wanting to make sure that it is still doing fine. When analysing older data sets, this parameter can be set to other timestamps so that the number of positions, distance travelled and detected events can be calculated in realtion to it.
+### Settings 
+**Reference time (`time_now`):** reference timestamp towards which all analyses are performed. Generally (and by default) this is NOW, especially if in the field and looking for one or the other animal or wanting to make sure that it is still doing fine. When analysing older data sets, this parameter can be set to other timestamps so that the number of positions, distance traveled and detected events can be calculated in relation to it.
 
-**Voltage variable name (`volt_name`):** Tags of different manufacturers often return tag voltages with different column names. To allow for this flexibility the user has to exactly provide the name of this data attribute. Please be careful to account for possible points, underscores and capitalisation.
+**Voltage variable name (`volt_name`):** Tags of different manufacturers often return tag voltages with different column names. To allow for this flexibility the user has to exactly provide the name of this data attribute. Please be careful to account for possible points, underscores and capitalization. If unsure of attribute names or spelling, please run the previous App in your workflow and check the event_attributes in the App Output Details (green 'i'). For definitions of Movebank attributes please refer to the Movebank Attribute Dictionary (https://www.movebank.org/cms/movebank-content/movebank-attribute-dictionary). 
 
-**Migration buffer (last 7 days) (`mig7d_dist`):** user-defined distance that an animal of the respecitve species is expected to minimally move during up to 7 days during migration. This variable is used to define the event `migration` that is reported in the overview table. The default value is presently set to 100000 m = 100 km.
+**Migration buffer (last 7 days) (`mig7d_distKM`):** user-defined distance that an animal of the respective species is expected to minimally move during up to 7 days during migration. This variable is used to define the event `migration` that is reported in the overview table. The default value is presently set to 100 km. Units Km
 
-**Mortality buffer (last 7 days) (`dead7d_dist`):** user-defined distance that an animal of the respecitve species is expected to minimally move during 7 days if it is alive. Take into account the data resolution, which can also miss longer displacements. This variable is used to define the event `dead` that is reported in the overview table. The default value is presently set to 100 m.
+**Mortality buffer (last 7 days) (`dead7d_dist`):** user-defined distance that an animal of the respective species is expected to minimally move during 7 days if it is alive. Take into account the data resolution, which can also miss longer displacements. This variable is used to define the event `dead` that is reported in the overview table. The default value is presently set to 100 m. Units M
 
-### Null or error handling:
+### Changes in output data
+The input data remains unchanged.
+
+### Most common errors
+
+### Null or error handling
 **Setting `time_now`:** If this parameter is left empty (NULL) the reference time is set to NOW. The present timestamp is extracted in UTC from the MoveApps server system.
 
 **Setting `volt_name`:** If this parameter is left empty or the provided column name is not available in the data set, the volatege column in the overview table is set to NA. Respective warning messages are provided.
@@ -35,5 +48,3 @@ moveStack in Movebank format
 **Setting `mig7d_dist`:** The parameter has a explicit default value, so NULL or non-numeric values are not possible and will give an error.
 
 **Setting `dead7d_dist`:** The parameter has a explicit default value, so NULL or non-numeric values are not possible and will give an error.
-
-**Data:** The data are not manipulated in this App, but used to provide an overview table. So that a possible Workflow can be continued after this App, the input data set is returned.
--- RFunction.R
+++ RFunction.R
@@ -1,130 +1,208 @@
-library('move')
-library('foreach')
+library('move2')
+library("dplyr")
+library("sf")
+library("geosphere")
 library('lutz')
-library('sf')
 library('grid')
 library('gridExtra')
 
-rFunction = function(time_now=NULL, volt_name=NULL, mig7d_dist, dead7d_dist, data, ...) {
+# data <- readRDS("./data/raw/input4_move2loc_LatLon.rds")
+# time_now <- max(mt_time(data))
+# names(data)
+# volt_name <- "eobs_battery_voltage"
+# mig7d_distKM <- 100
+# dead7d_dist <- 100
+
+rFunction = function(time_now=NULL, volt_name=NULL, mig7d_distKM, dead7d_dist, data) {
   
-  Sys.setenv(tz="UTC") 
+  Sys.setenv(tz="UTC")
+  
+  mig7d_dist <- mig7d_distKM*1000
   
   if (is.null(time_now)) time_now <- Sys.time() else time_now <- as.POSIXct(time_now,format="%Y-%m-%dT%H:%M:%OSZ",tz="UTC")
   
-  ids <- namesIndiv((data))
-  data_spl <- move::split(data)
-  
-  #do this before make.names to keep relation to previous App Cargo Agent
-  if (is.null(volt_name))
-  {
-    voltE <- rep(NA,length(ids))
+  if (is.null(volt_name)) {
+    data$noVolt <- NA
+    volt_name <- "noVolt"
     logger.info("No specification of voltage variable given, thus NA returned in voltage column of the overview table.")
-  } else if (volt_name %in% names(data))  
-  {
-    voltE <- foreach(datai = data_spl, .combine=c) %do% {
-      tail(datai@data[,volt_name],1)}
-  } else 
-  {
-    voltE <- rep(NA,length(ids))
+  } 
+  if (all(!is.null(volt_name) & !volt_name %in% names(data))){
+    data$noVolt <- NA
+    volt_name <- "noVolt"
     logger.info("Your provided tag voltage variable name does not exist in the data. Please double check and try again. NAs are returned in the voltage column of your overview table.")
   }
   
-  names(data) <- make.names(names(data),allow_=FALSE)
-  #add animal attributes to @data object
-  data.df <- as.data.frame(data)
-  ixo <- which(names(data.df) %in% names(data))
-  data@data <- cbind(data@data,data.df[,-ixo])
-  
-  data_spl <- move::split(data)
-  if (any(names(data@data)=="tag.local.identifier"))
-  {
-    tags <- foreach(datai = data_spl, .combine=c) %do% {
-      datai@data$tag.local.identifier[1]
-    }
-  } else tags <- rep(NA,length(ids))
-  
-  time0 <- foreach(datai = data_spl, .combine=c) %do% {
-    as.character(min(timestamps(datai)))
+  # table parameters
+  trkIDcolnm <- mt_track_id_column(data)
+  ## get tag names or set to NA
+  if(any(names(mt_track_data(data))=="tag_local_identifier")){
+  }else{
+    data <- data %>% mutate_track_data(tag_local_identifier=NA)
   }
-  timeE <- foreach(datai = data_spl, .combine=c) %do% {
-    as.character(max(timestamps(datai)))
-  }
-  timeE_local <- foreach(datai = data_spl, .combine=c) %do% {
-    timeEi <- max(timestamps(datai))
-    coo <- coordinates(datai[timestamps(datai)==timeEi][1,])
+  
+  getoverview <- function(x){
+    ## get local timezone for last position (probably posible to code more efficiently...)
+    timeEi <- max(mt_time(x))
+    coo <- st_coordinates(x %>% filter(mt_time()==max(mt_time())))
     timeEi_tz <- lutz::tz_lookup_coords(coo[,2],coo[,1], method="accurate")
     timeEi_offset <- lutz::tz_offset(as.Date(timeEi),timeEi_tz)
-    as.character(as.POSIXct(timeEi)+(timeEi_offset$utc_offset_h*3600))
+    locl_time <- as.character(timeEi+(timeEi_offset$utc_offset_h*3600))
+    ## tracks of last 24h and last 7 days 
+    trk24h <- x %>% filter(mt_time(x) <= time_now & mt_time(x) >= time_now-(24*60*60))
+    trk7d <- x %>% filter(mt_time(x) <= time_now & mt_time(x) >= time_now-(7*24*60*60))
+    
+    df <- data.frame(
+      ids=unique(mt_track_id(x)),
+      tags=mt_track_data(x)$tag_local_identifier,
+      time0=as.character(min(mt_time(x))),
+      timeE=as.character(max(mt_time(x))),
+      timeE_local=locl_time,
+      voltE=pull(x,var=volt_name)[nrow(x)],
+      posis24h=nrow(trk24h),
+      posis7d=nrow(trk7d),
+      displ24h=if(nrow(trk24h)==0){NA}else{paste(round(sum(mt_distance(trk24h),na.rm=T)/1000,digits=3),"km")},
+      displ7d=if(nrow(trk7d)==0){NA}else{paste(round(sum(mt_distance(trk7d),na.rm=T)/1000,digits=3),"km")}
+    )
+    return(df)
   }
   
-  event <- foreach(datai = data_spl, .combine=c) %do% {
-    ix <- which(timestamps(datai) <= time_now & timestamps(datai) >= time_now-(7*24*60*60))
-    if (length(ix)>0) 
-    {
-      datai7d <- datai[ix,]
-      meanlon <- mean(coordinates(datai7d)[,1])
-      meanlat <- mean(coordinates(datai7d)[,2])
-      if (any(distVincentyEllipsoid(coordinates(datai7d),c(meanlon,meanlat))>mig7d_dist))
-      {
-        "migration"
-      } else if (all(distVincentyEllipsoid(coordinates(datai7d),c(meanlon,meanlat))<dead7d_dist))
-      {
-        "dead"
-      } else "-"
-    } else "no data"
+  overview_tb <- data%>%group_by(mt_track_id()) %>%
+    group_modify(~getoverview(.x))
+  overview_tb <- st_drop_geometry(overview_tb)
+  overviewTB <- rename(overview_tb, {{trkIDcolnm}}:= 1)
+  
+  ## get animal status (probably took a coding detour)
+  data <- dplyr::mutate(data, lon = st_coordinates(data)[,1], lat = st_coordinates(data)[,2])
+  datai7d <-
+    data %>%
+    group_by(mt_track_id()) %>%
+    filter(mt_time() <= time_now & mt_time() >= time_now-(7*24*60*60))%>% 
+    reframe(
+      meanlon = mean(lon),
+      meanlat = mean(lat)
+    )
+  
+  datai7d <- rename(datai7d, {{trkIDcolnm}}:= 1)
+  data <- left_join(data,datai7d, by=trkIDcolnm)
+  
+  evnt <- function(x){
+    if(!all(is.na(x$meanlat))){
+      if (any(distVincentyEllipsoid(st_coordinates(x),c(unique(x$meanlon),unique(x$meanlat)))>(mig7d_dist*1000))){
+        df <- data.frame(event="migration")
+      } else if (all(distVincentyEllipsoid(st_coordinates(x),c(unique(x$meanlon),unique(x$meanlat)))<dead7d_dist )){
+        df <- data.frame(event="dead")
+      } else{df <- data.frame(event="-")}
+    } else {df <- data.frame(event="no data")}
+    return(df)
   }
   
-  posis24h <- foreach(datai = data_spl, .combine=c) %do% {
-    ix <- which(timestamps(datai) <= time_now & timestamps(datai) >= time_now-(24*60*60))
-    length(ix)
-  }
-  posis7d <- foreach(datai = data_spl, .combine=c) %do% {
-    ix <- which(timestamps(datai) <= time_now & timestamps(datai) >= time_now-(7*24*60*60))
-    length(ix)
-  }
-  displ24h <- foreach(datai = data_spl, .combine=c) %do% {
-    ix <- which(timestamps(datai) <= time_now & timestamps(datai) >= time_now-(24*60*60))
-    if (length(ix)>0) paste(round(sum(distance(datai[ix,]))/1000,digits=3),"km") else NA
-  }
-  displ7d <- foreach(datai = data_spl, .combine=c) %do% {
-    ix <- which(timestamps(datai) <= time_now & timestamps(datai) >= time_now-(7*24*60*60))
-    if (length(ix)>0) paste(round(sum(distance(datai[ix,]))/1000,digits=3),"km") else NA
-  }
+  eventDF <- data %>%
+    group_by(mt_track_id()) %>%
+    group_modify(~evnt(.x))
+  eventDF <- rename(eventDF, {{trkIDcolnm}}:= 1)
   
-  overview <- data.frame(ids,tags,time0,timeE,timeE_local,voltE,posis24h,posis7d,displ24h,displ7d,event)
-  names(overview) <- c("Animal","Tag","First timestamp","Last timestamp","Last timestamp local tz","Tag voltage","N posi. 24h","N posi. 7d","Moved dist. 24h","Moved dist. 7d","Event 7d")
+  ## join tables
+  overviewTB <- left_join(overviewTB,eventDF, by=trkIDcolnm)
+  overviewTB <- overviewTB %>% as_tibble() %>% select(-c(1)) # drop trkIDcolnm column used for joining
+  overviewTB <- overviewTB %>% relocate(ids,tags,time0,timeE,timeE_local,voltE,posis24h,posis7d,displ24h,displ7d,event)
   
+  ## "cheating" to achive a quicker fix. converting tibble to data.frame so the code below works. Will update gradually to tidyverse
+  overviewDF <- as.data.frame(overviewTB)
+  names(overviewDF) <- c("Animal","Tag","First timestamp UTC","Last timestamp UTC","Last timestamp local tz","Tag voltage","N posi. 24h","N posi. 7d","Moved dist. 24h","Moved dist. 7d","Event 7d")
+  
+  ## plot table
   mytheme <- gridExtra::ttheme_default(
     core = list(fg_params=list(cex = 0.7)),
     colhead = list(fg_params=list(cex = 0.7)),
     rowhead = list(fg_params=list(cex = 0.65)))
-  tg <- tableGrob(overview, rows = seq_len(nrow(overview)),theme=mytheme)
   
-  fullheight <- convertHeight(sum(tg$heights), "cm", valueOnly = TRUE)
-  margin <- unit(0.51,"in")
-  margin_cm <- convertHeight(margin, "cm", valueOnly = TRUE)
-  a4height <- 21 - margin_cm #landscape
-  nrows <- nrow(tg)
-  npages <- ceiling(fullheight / a4height)
+  tblG <- tableGrob(overviewDF, rows = NULL, theme =mytheme)# ttheme_minimal())
+  fullwidth <- convertWidth(sum(tblG$widths), "cm", valueOnly = TRUE)
+  fullheight <- convertHeight(sum(tblG$heights), "cm", valueOnly = TRUE)
+  a4height <- 15 # leaving a margin
+  a4width <- 25 # leaving a margin
+  heights <- convertHeight(tblG$heights, "cm", valueOnly = TRUE)
+  cumheight <- cumsum(heights)
+  max_rows_per_page <- length(which(cumheight<a4height))
+  max_rows_per_page_splt <- max_rows_per_page/2
   
-  heights <- convertHeight(tg$heights, "cm", valueOnly = TRUE) 
-  rows <- cut(cumsum(heights), include.lowest = FALSE,
-              breaks = c(0, cumsum(rep(a4height, npages))))
+  if(fullwidth>a4width){ 
+    # Split the data into two parts
+    table_top <- overviewDF[, 1:5] #"Animal","Tag","First timestamp UTC","Last timestamp UTC","Last timestamp local tz"
+    table_bottom <- overviewDF[, c(1:2, 6:ncol(overviewDF))] #"Animal","Tag","Tag voltage","N posi. 24h","N posi. 7d","Moved dist. 24h","Moved dist. 7d","Event 7d"
+    
+    # Function to create a grob for a single table
+    create_table_grob <- function(overviewDF, rows) {
+      tableGrob(overviewDF[rows, ], rows = NULL, theme = mytheme)#ttheme_minimal())
+    }
+    
+    if((fullheight*2)>a4height){  
+      # split into multiple pages
+      # Calculate the number of pages needed
+      num_rows <- nrow(overviewDF)
+      num_pages <- ceiling(num_rows/(max_rows_per_page_splt))
+      
+      plot_pages <- list()
+      
+      for (page in 1:num_pages) {
+        start_row <- (page - 1) * max_rows_per_page_splt + 1
+        end_row <- min(page * max_rows_per_page_splt, num_rows)
+        
+        # Create grobs for top and bottom tables, including column names
+        top_page <- create_table_grob(table_top, c(0, start_row:end_row))
+        bottom_page <- create_table_grob(table_bottom, c(0, start_row:end_row))
+        
+        # Combine top and bottom tables vertically
+        page_grob <- arrangeGrob(top_page, bottom_page, ncol = 1, heights = c(0.5, 0.5))
+        
+        plot_pages[[page]] <- page_grob
+      }
+    }else{
+      # Create grobs for top and bottom tables
+      top_page <- create_table_grob(table_top, c(0:nrow(overviewDF)))
+      bottom_page <- create_table_grob(table_bottom, c(0:nrow(overviewDF)))
+      
+      page_grob <- arrangeGrob(top_page, bottom_page, ncol = 1, heights = c(0.5, 0.5))
+      
+      plot_pages <- list()
+      plot_pages[[1]] <- page_grob
+    }
+  }else{
+    # one table
+    if(fullheight>a4height){
+      # split into multiple pages
+      num_rows <- nrow(overviewDF)
+      num_pages <- ceiling(num_rows/max_rows_per_page)
+      
+      # Function to create a grob for a single table
+      create_table_grob <- function(overviewDF, rows) {
+        tableGrob(overviewDF[rows, ], rows = NULL, theme =mytheme)# ttheme_minimal())
+      }
+      
+      plot_pages <- list()
+      
+      for (page in 1:num_pages) {
+        start_row <- (page - 1) * max_rows_per_page + 1
+        end_row <- min(page * max_rows_per_page, num_rows)
+        
+        # Subset the grobs for the current page
+        single_page <- tableGrob(overviewDF[c(0, start_row:end_row),], rows = NULL, theme = mytheme)# ttheme_minimal())
+        plot_pages[[page]] <- single_page
+      }
+    }else{
+      plot_pages <- list()
+      plot_pages[[1]] <- tblG
+    }
+  }
   
-  groups <- split(seq_len(nrows), rows)
-  
-  gl <- lapply(groups, function(id) tg[id,])
-  
-  
-  pdf(paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"),"MorningReport_overviewTable.pdf"), paper = "a4r", width = 0, height = 0)
-  #pdf("MorningReport_overviewTable.pdf", paper = "a4r", width = 0, height = 0)
-  for(page in seq_len(npages)){
+  pdf(appArtifactPath("MorningReport_overviewTable.pdf"), width = 11.69, height = 8.27) # A4 size in landscape
+  for (page in plot_pages) {
     grid.newpage()
-    grid.rect(width=unit(29.7,"cm") - margin,
-              height=unit(21,"cm")- margin)
-    grid.draw(gl[[page]])
+    grid.draw(page)
   }
   dev.off()
+  
+  write.csv(overviewDF, row.names=F, file = appArtifactPath("MorningReport_overviewTable.csv"))
   
   return(data)
 }
